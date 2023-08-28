e35a88f5ee955eba384ed592f36d4a0291b40c85
liuguangyong <1250171563@qq.com>
2023-08-28 16:26:08 +0800

    win32 init

---
+++ src/Main.cpp
@@ -0,0 +1,138 @@
+#include "stdafx.h"
+
+int WINAPI WinMain(HINSTANCE hInstance,    //Main windows function
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd)
+{
+	// create the window
+	if (!InitializeWindow(hInstance, nShowCmd, FullScreen))
+	{
+		MessageBox(0, L"Window Initialization - Failed",
+			L"Error", MB_OK);
+		return 1;
+	}
+
+	// start the main loop
+	mainloop();
+
+	return 0;
+}
+
+// create and show the window
+bool InitializeWindow(HINSTANCE hInstance,
+	int ShowWnd,
+	bool fullscreen)
+{
+	if (fullscreen)
+	{
+		HMONITOR hmon = MonitorFromWindow(hwnd,
+			MONITOR_DEFAULTTONEAREST);
+		MONITORINFO mi = { sizeof(mi) };
+		GetMonitorInfo(hmon, &mi);
+
+		Width = mi.rcMonitor.right - mi.rcMonitor.left;
+		Height = mi.rcMonitor.bottom - mi.rcMonitor.top;
+	}
+
+	WNDCLASSEX wc;
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = WindowName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, L"Error registering class",
+			L"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	hwnd = CreateWindowEx(NULL,
+		WindowName,
+		WindowTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		Width, Height,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	if (!hwnd)
+	{
+		MessageBox(NULL, L"Error creating window",
+			L"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	if (fullscreen)
+	{
+		SetWindowLong(hwnd, GWL_STYLE, 0);
+	}
+
+	ShowWindow(hwnd, ShowWnd);
+	UpdateWindow(hwnd);
+
+	return true;
+}
+
+void mainloop() {
+	MSG msg;
+	ZeroMemory(&msg, sizeof(MSG));
+
+	while (Running)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				break;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			// run game code
+			//Update(); // update the game logic
+			//Render(); // execute the command queue (rendering the scene is the result of the gpu executing the command lists)
+		}
+	}
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd,
+	UINT msg,
+	WPARAM wParam,
+	LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE) {
+			if (MessageBox(0, L"Are you sure you want to exit?",
+				L"Really?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+			{
+				Running = false;
+				DestroyWindow(hwnd);
+			}
+		}
+		return 0;
+
+	case WM_DESTROY: // x button on top right corner of window was pressed
+		Running = false;
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd,
+		msg,
+		wParam,
+		lParam);
+}
---
+++ src/stdafx.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN    // Exclude rarely-used stuff from Windows headers.
+#endif
+
+#include <windows.h>
+#include <string>
+
+// Handle to the window
+HWND hwnd = NULL;
+
+// name of the window (not the title)
+LPCTSTR WindowName = L"BzTutsApp";
+
+// title of the window
+LPCTSTR WindowTitle = L"Bz Window";
+
+// width and height of the window
+int Width = 800;
+int Height = 600;
+
+// is window full screen?
+bool FullScreen = false;
+
+// we will exit the program when this becomes false
+bool Running = true;
+
+// create a window
+bool InitializeWindow(HINSTANCE hInstance,
+	int ShowWnd,
+	bool fullscreen);
+
+// main application loop
+void mainloop();
+
+// callback function for windows messages
+LRESULT CALLBACK WndProc(HWND hWnd,
+	UINT msg,
+	WPARAM wParam,
+	LPARAM lParam);
