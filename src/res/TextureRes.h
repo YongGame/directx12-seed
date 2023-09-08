854ac050dbec706ac669efe90ce93d9cc5525b96
liuguangyong <1250171563@qq.com>
2023-09-08 11:59:45 +0800

    注释

--- src/res/TextureRes.h
+++ src/res/TextureRes.h
@@ -4,8 +4,9 @@
 class TextureRes : public Res
 {
 public:
-    // g_CPU_CBV_SRV_UAV_DescriptorHeap 描述符堆的描述符句柄
+    // 纹理需要创建描述符
+    // g_CPU_CBV_SRV_UAV_DescriptorHeap 描述符堆的描述符句柄， 句柄是对资源的描述
     int handleIndex;
-    
+
     void create(LPTSTR name, D3D12_RESOURCE_DESC &textureDesc, BYTE *imageData, int imageBytesPerRow);
 };
